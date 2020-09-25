1e425832875967a4cf53052da71b5880c76d8a53
langat justus <justuslangat78@gmail.com>
2020-09-25 13:42:17 +0300

    configure the app with sqlachemy

--- guestbook.py
+++ guestbook.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
+form flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
+
 
 @app.route('/')
 def index():
