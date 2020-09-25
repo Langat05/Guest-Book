59c11568a520c2717bb0d678b3f8321d0a0c1b36
langat justus <justuslangat78@gmail.com>
2020-09-25 12:55:55 +0300

    modified the guestbook.py file

--- guestbook.py
+++ guestbook.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,13 @@ def index():
 @app.route('/sign')
 def sign():
 	return render_template('sign.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+	name = request.form['name']
+	comment = request.form['comment']
+
+	return render_template('index.html', name=name, comment=comment)
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
